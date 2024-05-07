7cd3836391faa23ada06b2567c1e8a8094cc4b50
Mike <mishakozlov74@gmail.com>
2024-05-08 01:19:56 +0200

    Fix gitignore

---
+++ Assets/Netick/Transports/FacepunchTransport/FacepunchTransportProvider.cs
@@ -0,0 +1,31 @@
+﻿using Netick.Unity;
+using Steamworks.Data;
+using UnityEngine;
+
+namespace Netick.Transports.FacepunchTransport {
+    [CreateAssetMenu(fileName = "FacepunchTransport", menuName = "Netick/Transport/FacepunchTransport", order = 2)]
+    public class FacepunchTransportProvider : NetworkTransportProvider {
+
+        [Tooltip("ive found that No Nagle provides the best latency for me")]
+        [SerializeField]
+        SendType SteamDataSendType = SendType.NoNagle;
+
+        [Tooltip("Keep this off if you are using the NoNagle send type. turning this on disables the nagle timer.")]
+        [SerializeField]
+        bool FlushMessages;
+
+        [Tooltip("Keep this off if you are using the NoNagle send type. turning this on disables the nagle timer.")]
+        [SerializeField]
+        LogLevel logLevel = LogLevel.Error;
+
+
+#if UNITY_EDITOR
+        public void OnValidate() {
+            FacepunchTransport.SteamSendType = SteamDataSendType;
+            FacepunchTransport.ForceFlush = FlushMessages;
+        }
+#endif
+
+        public override NetworkTransport MakeTransportInstance() => new FacepunchTransport(SteamDataSendType, FlushMessages, logLevel);
+    }
+}
