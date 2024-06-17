f37cec536a31074b9ba2385d3f328e5def03fafb
Milk-Drinker01 <camerondus@gmail.com>
2024-06-17 19:58:25 -0400

    fix double player spawn

--- Assets/Netick/Transports/Facepunch/Extras/SteamInitializer.cs
+++ Assets/Netick/Transports/Facepunch/Extras/SteamInitializer.cs
@@ -3,24 +3,35 @@ using System.Collections;
 using Steamworks;
 using UnityEngine;
 
-namespace Netick.Transports.Facepunch.Extras {
-    public class SteamInitializer : MonoBehaviour {
+namespace Netick.Transports.Facepunch.Extras
+{
+    public class SteamInitializer : MonoBehaviour
+    {
+        public static event Action OnInitializeCallbacks;
 
         [SerializeField]
         uint AppID = 480;
 
-        void Awake() {
-            if (!SteamClient.IsValid) {
+        void Awake()
+        {
+            if (!SteamClient.IsValid)
+            {
                 SteamClient.Init(AppID);
             }
 
             StartCoroutine(EnsureValidity());
         }
-        public static event Action OnInitializeCallbacks;
 
-        IEnumerator EnsureValidity() {
+        IEnumerator EnsureValidity()
+        {
             yield return new WaitUntil(() => SteamClient.IsValid);
             OnInitializeCallbacks?.Invoke();
+        }
+
+        private void OnDestroy()
+        {
+            if (SteamClient.IsValid)
+                SteamClient.Shutdown();
         }
     }
 }
--- Assets/NetickSteamDemos/DemoGame/Scripts/SteamFPSEventsHandler.cs
+++ Assets/NetickSteamDemos/DemoGame/Scripts/SteamFPSEventsHandler.cs
@@ -9,25 +9,6 @@ namespace Netick.Examples.Steam
         public Transform    SpawnPos;
         public GameObject   PlayerPrefab;
 
-        // This is called on the server and the clients when the scene has been loaded.
-        public override void OnSceneLoaded(NetworkSandbox sandbox)
-        {
-            if (sandbox.IsClient)
-                return;
-
-            for (int i = 0; i < sandbox.ConnectedPlayers.Count; i++)
-            {
-                if (sandbox.ConnectedPlayers[i].PlayerObject != null)
-                    continue;
-
-                var p          = sandbox.ConnectedPlayers[i];
-
-                var spawnPos   = SpawnPos.position + Vector3.left * (i);
-                var player     = sandbox.NetworkInstantiate(PlayerPrefab, spawnPos, Quaternion.identity, p).GetComponent<SteamFPSController>();
-                p.PlayerObject = player.gameObject;
-            }
-        }
-
         // This is called on the server when a client has connected.
         public override void OnPlayerConnected(NetworkSandbox sandbox, Netick.NetworkPlayer client)
         {
